d00246d766b59854370ef503732f7019a1cb4f40
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2019-12-13 08:37:32 +0100

    Have added XML/XSD material

---
+++ fuse-parent/osgi-parent/customers/customers-service/src/main/java/fr/simplex_software/red_hat/fuse/osgi/standalone/customers/data/services/CustomerServiceImpl.java
@@ -0,0 +1,50 @@
+package fr.simplex_software.red_hat.fuse.osgi.standalone.customers.data.services;
+
+import fr.simplex_software.red_hat.fuse.osgi.standalone.customers.data.entities.*;
+
+import javax.persistence.*;
+import javax.transaction.*;
+import java.math.*;
+import java.util.*;
+
+public class CustomerServiceImpl implements CustomerDataService
+{
+  @PersistenceContext(unitName = "customers")
+  private EntityManager entityManager;
+
+  public void setEntityManager(EntityManager entityManager)
+  {
+    this.entityManager = entityManager;
+  }
+
+  @Override
+  @Transactional(Transactional.TxType.SUPPORTS)
+  public List<Customer> getCustomers()
+  {
+    TypedQuery<Customer> query = entityManager.createQuery("SELECT c FROM Customer c", Customer.class);
+    return query.getResultList();
+  }
+
+  @Override
+  @Transactional(Transactional.TxType.SUPPORTS)
+  public Customer getCustomer(BigInteger id)
+  {
+    TypedQuery<Customer> query = entityManager.createQuery("SELECT c FROM Customer c WHERE c.id=:id", Customer.class);
+    query.setParameter("id", id);
+    return query.getSingleResult();
+  }
+
+  @Override
+  @Transactional(Transactional.TxType.REQUIRES_NEW)
+  public void createCustomer(Customer customer)
+  {
+    entityManager.persist(customer);
+  }
+
+  @Override
+  @Transactional(Transactional.TxType.REQUIRES_NEW)
+  public void removeCustomer(Customer customer)
+  {
+    entityManager.remove(getCustomer(customer.getCustomerId()));
+  }
+}
